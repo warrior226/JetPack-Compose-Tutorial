669a78c79ec4887f3026b25878c1f13612ce0c42
prayaisse <prayaisse@ticanalyse.org>
2025-05-05 16:11:01 +0000

    Box- Jetpack Compose

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
@@ -13,6 +15,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -21,8 +25,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetutorial.ui.theme.JetPackComposeTutorialTheme
 import com.example.jetpackcomposetutorial.ui.theme.Typography
 
@@ -32,17 +38,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetPackComposeTutorialTheme {
-                Scaffold(modifier = Modifier.fillMaxSize(), containerColor =Color.White) { innerPadding ->
-
-                    Row(
-                        modifier = Modifier.fillMaxSize()
-                            .padding(innerPadding),
-                        horizontalArrangement = Arrangement.Start,
-
-                        ) {
-                        CustomItem(weight = 3f, color = MaterialTheme.colorScheme.secondary)
-                        CustomItem(weight = 1f)
-                    }
+                Surface (color=MaterialTheme.colorScheme.background) {
+                    Greeting()
                 }
             }
         }
@@ -50,44 +47,30 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!!",
-        modifier = modifier
-    )
-}
+fun Greeting() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopCenter
+    ){
+        Box(
+            modifier = Modifier
+                .background(Color.Blue)
 
-@Composable
-fun CustomText(text:String){
-    Text(
-        text =text,
-        style= Typography.displaySmall
-    )
-}
-
-@Composable
-fun RowScope.CustomItem(weight:Float, color:Color=MaterialTheme.colorScheme.primary){
-    Surface(modifier = Modifier
-        .width(200.dp)
-        .height(50.dp)
-        .weight(weight),
-        color = color){
-
+        ){
+            //Inside a Box Composable , elements are on top of each other
+            Box(modifier = Modifier.height(250.dp).width(250.dp).background(Color.Red))
+            Box(modifier = Modifier.height(150.dp).width(150.dp).background(Color.Green))
+            Text("I love android", fontSize =50.sp);
+        }
     }
 }
+
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetPackComposeTutorialTheme {
-
-        Row(
-            modifier = Modifier.fillMaxSize(),
-            horizontalArrangement = Arrangement.Start,
-
-        ) {
-            CustomItem(weight = 3f, color = MaterialTheme.colorScheme.secondary)
-            CustomItem(weight = 1f)
-        }
+        Greeting()
     }
 }
