152ac2430dd2aae2ed8fa35cedddf5e5b28b6d01
prayaisse <prayaisse@ticanalyse.org>
2025-05-05 23:23:23 +0000

    Text Customization -JetPack Compose

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -5,32 +5,29 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.ParagraphStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetutorial.ui.theme.JetPackComposeTutorialTheme
-import com.example.jetpackcomposetutorial.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,29 +45,65 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.TopCenter
-    ){
-        Box(
-            modifier = Modifier
-                .background(Color.Blue)
-
-        ){
-            //Inside a Box Composable , elements are on top of each other
-            Box(modifier = Modifier.height(250.dp).width(250.dp).background(Color.Red))
-            Box(modifier = Modifier.height(150.dp).width(150.dp).background(Color.Green))
-            Text("I love android", fontSize =50.sp);
-        }
+    Column(modifier = Modifier.fillMaxSize()) {
+        Text(
+            modifier =Modifier.background(MaterialTheme.colorScheme.primary)
+                .padding(16.dp)
+                .width(500.dp)
+            ,
+            text = stringResource(R.string.app_name),
+            color=Color.White,
+            fontSize =MaterialTheme.typography.headlineSmall.fontSize,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.End
+        )
     }
 }
 
+//Composable to apply Style on each character
+@Composable
+fun CustomText() {
+    // Access colorScheme within the @Composable function's body
+    val primaryColor = MaterialTheme.colorScheme.primary
 
+    Text(
+        buildAnnotatedString {
+            withStyle(style = ParagraphStyle()) {
+                withStyle(
+                    style = SpanStyle(
+                        color = primaryColor, // Use the variable instead
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                ) {
+                    append("A")
+                }
+                append("B")
+                append("C")
+                append("D")
+            }
+        },
+        modifier = Modifier.width(200.dp)
+    )
+}
+
+
+@Composable
+fun CustomText3(){
+Text(text = "Hello World".repeat(100),maxLines = 2, overflow = TextOverflow.Visible)
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetPackComposeTutorialTheme {
-        Greeting()
+        Column(modifier = Modifier.fillMaxSize()) {
+            CustomText()
+            //  Greeting()
+            CustomText3()
+
+        }
+
     }
 }
