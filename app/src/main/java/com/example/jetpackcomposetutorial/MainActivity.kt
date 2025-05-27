940949633434ccd848fbbb467bc80a46d306ea97
prayaisse <prayaisse@ticanalyse.org>
2025-05-27 12:50:50 +0000

    How to save data throug change configuration using onSaveInstanceState

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -5,70 +5,57 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
-import androidx.compose.foundation.layout.systemBars
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import com.example.jetpackcomposetutorial.di.hilt.MyViewModel
-import com.example.jetpackcomposetutorial.di.manual.ManualApplication
-import com.example.jetpackcomposetutorial.screen.SetupNavGraph
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpackcomposetutorial.ui.theme.JetPackComposeTutorialTheme
-import org.koin.androidx.compose.koinViewModel
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
+
+    private var randomNumber=Random.nextInt(20)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
+        if(savedInstanceState!=null){
+            randomNumber=savedInstanceState.getInt("random")
+        }
+        enableEdgeToEdge()
         setContent {
             JetPackComposeTutorialTheme {
-                var navHostController: NavHostController
-                val hiltViewModel = hiltViewModel<MyViewModel>()
-                val  nom= hiltViewModel.getInfo()[0]
-                val prenom= hiltViewModel.getInfo()[1]
-                val koinViewModel=koinViewModel<com.example.jetpackcomposetutorial.di.koin.MyViewModel>()
-                val manualViwModel= viewModel<MyViewModel>(
-                    factory=ManualApplication.manualAppModule.myViewModelFactory
-                )
-
-                Scaffold(
-                    topBar = {
-                        Box(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .background(Color.Transparent)
-                                .padding(WindowInsets.statusBars.asPaddingValues())
-                        ) {
-                        }
-                    }
-                ) { innerPadding ->
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding)
-                            .background(Color.White)
-                    ) {
-                        // Your main content
-                        navHostController= rememberNavController()
-                        SetupNavGraph(navHostController)
-                    }
+                Scaffold(modifier = Modifier.background(Color.White).fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name="$randomNumber",
+                        modifier=Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("random",randomNumber)
+    }
+
 }
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name!")
+}
+
+//@Composable
+//@Preview(showBackground = true)
+//fun GreetingPreview(name: String, modifier: Modifier = Modifier) {
+//    Text(text = "Hello $name!")
+//}
 
 
 
