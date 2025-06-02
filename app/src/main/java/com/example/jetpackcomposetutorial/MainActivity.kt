d7462f3f02e32d5d40f44f646d2f391b89c8fbbb
prayaisse <prayaisse@ticanalyse.org>
2025-06-02 18:12:52 +0000

    Mise en page Scafold,TopAppBar,NavigationBar

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jetpackcomposetutorial
 
 import android.content.Intent
+import android.graphics.Paint
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
@@ -8,6 +9,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -24,11 +26,31 @@ import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
@@ -48,53 +70,82 @@ class MainActivity : ComponentActivity() {
 
     private var randomNumber=Random.nextInt(20)
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            JetPackComposeTutorialTheme {
-                val navController= rememberNavController()
-                NavHost(
-                    navController=navController,
-                    startDestination =Route.ScreenA
-                ) {
-                    composable<Route.ScreenA> {
-                        ScreenA(onNavigateToScreenB ={
-                            navController.navigate(Route.ScreenB)
-                        })
-                    }
-
-                    composable<Route.ScreenB>{
-                        ScreenB(onNavigateToScreenC ={text,number->
-                            navController.navigate(Route.ScreenC(
-                                myText = text,
-                                myNumber = number
-                            ))
-                        })
-                    }
-
-                    composable<Route.ScreenC> {
-                        val args=it.toRoute<Route.ScreenC>()
-                        ScreenC(
-                            args.myText,
-                            args.myNumber,
-                            onNavigateToScreenD = {nom,prenom->
-                                navController.navigate(Route.ScreenD(
-                                    nom=nom,
-                                    prenom=prenom
-                                ))
+            val menusList=listOf("Accueil","Paramètres")
+            var menuSelected by rememberSaveable { mutableStateOf(menusList[0]) }
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        title = {
+                            Text("My App")
+                        },
+                        navigationIcon = {
+                            Icon(imageVector = Icons.Default.ShoppingCart, contentDescription = "Logo")
+                        },
+                        actions = {
+                            IconButton(
+                                onClick = {}
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.AccountCircle,
+                                    contentDescription = "Profile"
+                                )
                             }
-                        )
-                    }
+                            IconButton(
+                                onClick = {}
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.MoreVert,
+                                    contentDescription = "Menus plus"
+                                )
+                            }
+                        }
+                    )
+                },
+                bottomBar = {
+                    NavigationBar {
+                        menusList.forEach {menu->
+                            NavigationBarItem(
+                                selected = menuSelected==menu,
+                                onClick = {
+                                    menuSelected=menu
+                                },
+                                icon ={
+                                   Icon(imageVector=Icons.Default.Home, contentDescription = "Icon")
 
-                    composable<Route.ScreenD>{
-                        val args=it.toRoute<Route.ScreenD>()
-                        ScreenD(
-                            args.nom,
-                            args.prenom
-                        )
+                                },
+                                label={
+                                    Text(menu)
+                                }
+                            )
 
+                        }
                     }
+                },
+                floatingActionButton = {
+                    ExtendedFloatingActionButton(
+                        onClick = {}
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Add,
+                            contentDescription = "Ajouter"
+                        )
+                        Text("Ajouter")
+                    }
+                }
+            ) {
+                paddingValue->
+                Box(
+                    modifier = Modifier
+                        .padding(paddingValue)
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ){
+                    Text(text=menuSelected)
                 }
             }
         }
